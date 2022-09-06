a432d9a649399c3a5685fe45d92256d5f94eb237
Peiman <peiman_hp@yahoo.com>
2022-09-06 16:53:24 +0430

    while statement added to player choice

--- script.js
+++ script.js
@@ -8,14 +8,23 @@ let palyerWins;
 let computerWins;
 
 // player choice:
-let playerInput = prompt(
-  "Enter first letter of your object to play (Between R & P & S)"
-);
-if (playerInput == "r" || playerInput == "R") playerChoice = 0;
-else if (playerInput == "p" || playerInput == "P") playerChoice = 1;
-else if (playerInput == "s" || playerInput == "S") playerChoice = 2;
-else {
-  alert(`Please select a letter between "R" or "P" or "S"`);
+let isTrueSelection = 0;
+while (isTrueSelection == 0) {    
+    let playerInput = prompt(
+        "Enter first letter of your object to play (Between R & P & S)"
+    );
+    if (playerInput == "r" || playerInput == "R") {
+        playerChoice = 0;
+        isTrueSelection = 1;
+    } else if (playerInput == "p" || playerInput == "P") {
+        playerChoice = 1;
+        isTrueSelection = 1;
+    } else if (playerInput == "s" || playerInput == "S") {
+        playerChoice = 2;
+        isTrueSelection = 1;
+    } else {
+        alert(`Please select a letter between "R" or "P" or "S"`);
+    }
 }
 console.log(`Player choice: ${playerChoice}`);
 
