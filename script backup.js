fdc41469fd15c527261aa39f721c29640f5fb8c9
Peiman <peiman_hp@yahoo.com>
2022-10-02 00:21:20 +0330

    user buttons working

--- script backup.js
+++ script backup.js
@@ -2,40 +2,47 @@ const rock = 0;
 const paper = 1;
 const scissors = 2;
 let playerChoice;
-let computerChoice;
 let playerScore = 0;
 let computerScore = 0;
 
-// player choice:
-const rockBtn = document.getElementById("rock");
-const paperBtn = document.getElementById("paper");
-const scissorsBtn = document.getElementById("scissors");
-rockBtn.addEventListener("click", playGame);
-paperBtn.addEventListener("click", playGame);
-scissorsBtn.addEventListener("click", playGame);
+while (playerScore != 5 || computerScore != 5) {
+  // player choice:
+  let isTrueSelection = 0;
+  while (isTrueSelection == 0) {
+    let playerInput = prompt(
+      `Enter first letter of your object to play (R & P & S)`
+    );
+    if (playerInput.toLowerCase() == "r") {
+      playerChoice = 0;
+      isTrueSelection = 1;
+    } else if (playerInput.toLowerCase() == "p") {
+      playerChoice = 1;
+      isTrueSelection = 1;
+    } else if (playerInput.toLowerCase() == "s") {
+      playerChoice = 2;
+      isTrueSelection = 1;
+    } else if (playerInput.toLowerCase() == "exit") {
+      isTrueSelection = 2;
+    } else {
+      alert(`Please select a letter between "R" or "P" or "S"`);
+    }
+  }
+  console.log(`Player choice: ${playerChoice}`);
+  if (isTrueSelection == 2) break; // breaks loops with typing "exit"
 
-// computer choice:
-function computerPlay() {
-  let randomPlay = Math.floor(Math.random() * 3);
-  if (randomPlay == 0) return "ROCK";
-  else if (randomPlay == 2) return "PAPER";
-  else return "SCISSORS";
-}
-
-// lets play!
-function playGame(playerChoice, computerChoice) {
-  playerChoice = this.querySelector("h4").textContent;
-  console.log(`Your choice: ${playerChoice}`);
-  computerChoice = computerPlay();
+  // computer choice:
+  let computerChoice = Math.floor(Math.random() * 3);
   console.log(`Computer choice: ${computerChoice}`);
-  if (playerChoice == 'ROCK' && computerChoice == 'ROCK') {
+
+  // lets play!
+  if (playerChoice == 0 && computerChoice == 0) {
     console.log(`it's tie
   Rock ties with rock`);
     alert(`it's tie
   Rock ties with rock
   Player: ${playerScore}
   Computer: ${computerScore}`);
-  } else if (playerChoice == 'ROCK' && computerChoice == 'PAPER') {
+  } else if (playerChoice == 0 && computerChoice == 1) {
     computerScore++;
     console.log(`You lost!
   Rock is beaten by Paper`);
@@ -43,7 +50,7 @@ function playGame(playerChoice, computerChoice) {
   Rock is beaten by Paper
   Player: ${playerScore}
   Computer: ${computerScore}`);
-  } else if (playerChoice == 'ROCK' && computerChoice == 'SCISSORS') {
+  } else if (playerChoice == 0 && computerChoice == 2) {
     playerScore++;
     console.log(`You Won!
   Rock beats scissors`);
@@ -51,7 +58,7 @@ function playGame(playerChoice, computerChoice) {
   Rock beats scissors
   Player: ${playerScore}
   Computer: ${computerScore}`);
-  } else if (playerChoice == 'PAPER' && computerChoice == 'ROCK') {
+  } else if (playerChoice == 1 && computerChoice == 0) {
     playerScore++;
     console.log(`You Won!
   Paper beats rock`);
@@ -59,14 +66,14 @@ function playGame(playerChoice, computerChoice) {
   Paper beats rock
   Player: ${playerScore}
   Computer: ${computerScore}`);
-  } else if (playerChoice == 'PAPER' && computerChoice == 'PAPER') {
+  } else if (playerChoice == 1 && computerChoice == 1) {
     console.log(`it's tie
   Paper ties with Paper`);
     alert(`it's tie
   Paper ties with Paper
   Player: ${playerScore}
   Computer: ${computerScore}`);
-  } else if (playerChoice == 'PAPER' && computerChoice == 'SCISSORS') {
+  } else if (playerChoice == 1 && computerChoice == 2) {
     computerScore++;
     console.log(`You lost!
   Paper is beaten by scissors`);
@@ -74,7 +81,7 @@ function playGame(playerChoice, computerChoice) {
   Paper is beaten by scissors
   Player: ${playerScore}
   Computer: ${computerScore}`);
-  } else if (playerChoice == "SCISSORS" && computerChoice == "ROCK") {
+  } else if (playerChoice == 2 && computerChoice == 0) {
     computerScore++;
     console.log(`You lost!
   Scissors is beaten by rock`);
@@ -82,7 +89,7 @@ function playGame(playerChoice, computerChoice) {
   Scissors is beaten by rock
   Player: ${playerScore}
   Computer: ${computerScore}`);
-  } else if (playerChoice == "SCISSORS" && computerChoice == "PAPER") {
+  } else if (playerChoice == 2 && computerChoice == 1) {
     playerScore++;
     console.log(`You Won!
   Scissors beats paper`);
@@ -90,7 +97,7 @@ function playGame(playerChoice, computerChoice) {
   Scissors beats paper
   Player: ${playerScore}
   Computer: ${computerScore}`);
-  } else if (playerChoice == "SCISSORS" && computerChoice == "SCISSORS") {
+  } else if (playerChoice == 2 && computerChoice == 2) {
     console.log(`it's tie
   Scissors ties with scissors`);
     alert(`it's tie
@@ -98,13 +105,15 @@ function playGame(playerChoice, computerChoice) {
   Player: ${playerScore}
   Computer: ${computerScore}`);
   }
-}
 
-// Final winner
-if (playerScore == 5) {
-  alert("You won the game!");
-  console.log("You won the game!");
-} else if (computerScore == 5) {
-  alert("You lost the game!");
-  console.log("You lost the game!");
+  // Final winner
+  if (playerScore == 5) {
+    alert("You won the game!");
+    console.log("You won the game!");
+    break;
+  } else if (computerScore == 5) {
+    alert("You lost the game!");
+    console.log("You lost the game!");
+    break;
+  }
 }
